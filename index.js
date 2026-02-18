548880972724fc3041d5337d9cfd5cf77606eadd
Abdulaziz Hamid <abdulaziz5536h@gmail.com>
2026-02-18 19:06:15 +0300

    commit

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express=require('express');
 const app=express();
 app.use(express.json());
 const mongoose=require('mongoose');
+
 const PORT=3000;
+
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 });
