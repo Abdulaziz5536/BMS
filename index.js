45276a141c36d8f468dbdda4f085090b5e3e9645
emranbehja11 <135129450+emranbehja11@users.noreply.github.com>
2026-02-18 18:53:39 +0300

    Auth

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const a = 1
+const express=require('express');
+const app=express();
+const mongoose=require('mongoose');
+const PORT=3000;
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
