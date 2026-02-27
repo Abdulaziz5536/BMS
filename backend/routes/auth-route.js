defcda8986d19b6698aa17925602b988c2759c3a
reacklesscoder <reacklesscoder@gmail.com>
2026-02-27 12:41:52 +0300

    commit

--- backend/routes/auth-route.js
+++ backend/routes/auth-route.js
@@ -2,18 +2,18 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/auth-model');
 
-router.post('/register', async (req,res) => {
+router.post('/signup', async (req,res) => {
   const {name,email,password} = req.body;
 
   const existingUser = await User.findOne({email});
   if(existingUser){
-    return res.status.json({error:"user already exists"});
+    return res.status(400).json({error:"user already exists"});
   }
   if(!email.includes("@")){
-    return res.status.json({error:"email is invalid"});
+    return res.status(400).json({error:"email is invalid"});
   }
   if(password.length !== 6){
-    return res.status.json({error:"password should be 4 digits"});
+    return res.status(400).json({error:"password should be 4 digits"});
   }
 
   const user = await User.create({name,email,password});
@@ -27,10 +27,10 @@ router.post('/login',async (req,res) => {
   const existingUser = await User.findOne({email,password});
 
   if(!existingUser){
-    return res.json({error:"user does not exist"});
+    return res.status(400).json({error:"user does not exist"});
   }
   if(existingUser.password !== password){
-    return res.json({error:"password is not correct"});
+    return res.status(400).json({error:"password is not correct"});
   }
 
   res.json({message:"logged in successfully"});
