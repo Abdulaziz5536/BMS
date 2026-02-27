279aa50f16f6db058e14894fa071694d347b3793
Abdulaziz Hamid <abdulaziz5536h@gmail.com>
2026-02-27 11:41:37 +0300

    commit

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,16 @@ const express=require('express');
 const app=express();
 app.use(express.json());
 const mongoose=require('mongoose');
+const authRouter = require('./routes/auth-route');
+app.use(express.json());
 
 const PORT=3000;
 
 mongoose.connect("mongodb://127.0.0.1:27017/BMS").then(() => console.log("mongoDB is connected"))
 .catch(err => console.log(err));
+
+
+app.use(authRouter);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
