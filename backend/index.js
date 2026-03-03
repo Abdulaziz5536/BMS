666e646ce09021479d9b0415d5baf802c54f8bfe
Emran Abdulwahab <yourgithubemail@example.com>
2026-03-03 12:07:12 +0300

    added database

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,20 @@
 const express=require('express');
 const app=express();
-app.use(express.json());
 const mongoose=require('mongoose');
+require('dotenv').config();
 const authRouter = require('./routes/auth-route');
 app.use(express.json());
 const cors = require('cors');
+
+
+app.use(express.json());
 app.use(cors());
+
+
 
 const PORT=3000;
 
-mongoose.connect("mongodb://127.0.0.1:27017/BMS").then(() => console.log("mongoDB is connected"))
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("mongoDB is connected"))
 .catch(err => console.log(err));
 
 
