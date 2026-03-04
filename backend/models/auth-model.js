75b12bb1007c758349f44e569ed656435476a21c
Abdulaziz Hamid <abdulaziz5536h@gmail.com>
2026-03-04 17:08:56 +0300

    c

--- backend/models/auth-model.js
+++ backend/models/auth-model.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
-    name:String,
-    email:String,
-    password:String,
+    name:{
+        type:String
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
 })
 module.exports = mongoose.model('User',userSchema);
 
